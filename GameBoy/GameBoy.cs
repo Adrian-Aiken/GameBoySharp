bd6a126d25633a88605c4d46a2aa00c9c4960032
Adrian Aiken <goatmancer@gmail.com>
2015-11-14 16:39:36 -0800

    Changed project from Library to Forms; Added (very) basic window items & operations

--- GameBoy/GameBoy.cs
+++ GameBoy/GameBoy.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GameBoy
 {
-    public static class GameBoy
+    public partial class GameBoy : Form
     {
+        public GameBoy()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            this.Menu = new MainMenu();
+            MenuItem item;
+
+            item = new MenuItem("File");
+            item.MenuItems.Add(new MenuItem("Open ROM...", new EventHandler(OpenRom)));
+            item.MenuItems.Add(new MenuItem("Exit", new EventHandler(CloseGameboy)));
+            this.Menu.MenuItems.Add(item);
+
+            item = new MenuItem("About");
+            this.Menu.MenuItems.Add(item);
+        }
+
+        private void OpenRom(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CloseGameboy(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
